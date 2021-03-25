91797079a6d36b7d6ec18cd26d3d31796e42567f
PavloSukhylin <supana1976@gmail.com>
2021-03-25 22:22:01 +0300

    Correct work.

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,13 @@
 public class Test {
     public static void main(String[] args) {
-        int[] mass = {2,3,6,2,9,7,1,3};
+        int[] mass = {2,4,5,9,0,4,2,8,5,7,1,4,2};
         int lenght = mass.length;
         int quantity = lenght;
         for (int i = 0; i < lenght-1; i++) {
             for (int j = i+1; j < lenght; j++) {
                 if (mass[i] == mass[j]){
                     quantity = quantity-1;
+                    break;
                 }
             }
         }
